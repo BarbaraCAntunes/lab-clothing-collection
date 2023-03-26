cac230a61f3bf89e499c5683a79548c9811eee65
Barbara <barbaracantunes@gmail.com>
2023-03-25 22:23:42 -0300

    Add componente "Criar coleção"

---
+++ src/app/pages/post-login-layout/create-collection/create-collection.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateCollectionComponent } from './create-collection.component';
+
+describe('CreateCollectionComponent', () => {
+  let component: CreateCollectionComponent;
+  let fixture: ComponentFixture<CreateCollectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreateCollectionComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CreateCollectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/post-login-layout/create-collection/create-collection.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-collection',
+  templateUrl: './create-collection.component.html',
+  styleUrls: ['./create-collection.component.css']
+})
+export class CreateCollectionComponent implements OnInit {
+  
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+  }
+
+}
