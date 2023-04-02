e0cb4a5036cb2069624dc5cbbab1ffb6f6baa3ff
Barbara <barbaracantunes@gmail.com>
2023-04-02 20:14:29 -0300

    Adiciona lógina e revisa outros códigos

--- src/app/pages/pre-login-layout/login/login.component.ts
+++ src/app/pages/pre-login-layout/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth-service.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -6,60 +6,61 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-  loginForm: FormGroup;
-  emailErrorMessage = '';
-  passwordErrorMessage = '';
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  emailErrorMessage: string = '';
+  passwordErrorMessage: string = '';
 
   constructor(
-    private authService: AuthService, 
-    private router: Router, 
+    private authService: AuthService,
+    private router: Router,
     private formBuilder: FormBuilder
-  ) { 
+  ) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
     });
   }
 
-    onInputChange() {
-      this.emailErrorMessage = '';
-      this.passwordErrorMessage = '';
-    }
+  ngOnInit() {}
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 
   onSubmit() {
-    const emailControl = this.loginForm.get('email');
-    const passwordControl = this.loginForm.get('password');
-    
-    if (!emailControl) {
-      this.emailErrorMessage = 'Campo e-mail é obrigatório';
-    } else {
-      const email = emailControl.value;
+    const { email, password } = this.loginForm.value;
 
-      if (emailControl.invalid) {
-        this.emailErrorMessage = 'Digite um e-mail válido';
-      } else if (!this.authService.isEmailRegistered(email)) {
-        this.emailErrorMessage = 'E-mail não cadastrado';
+    if (this.email?.errors?.['required']) {
+      this.emailErrorMessage = '❌ Por favor, preencha o campo de e-mail.';
+    } else if (this.email?.errors?.['email']) {
+      this.emailErrorMessage = '❌ Por favor, insira um e-mail válido.';
+    } else {
+      this.emailErrorMessage = '';
+
+      if (this.password?.errors?.['required']) {
+        this.passwordErrorMessage = '❌ Por favor, preencha o campo de senha.';
+      } else if (this.password?.errors?.['minlength']) {
+        this.passwordErrorMessage = '❌ A senha deve ter 8 caracteres.';
+      } else {
+        this.passwordErrorMessage = '';
+
+        this.authService
+          .authenticateUser(email, password)
+          .then((isAuthenticated) => {
+            if (isAuthenticated) {
+              this.router.navigate(['/home']);
+            } else {
+              this.emailErrorMessage = '❌ E-mail ou senha inválidos.';
+            }
+          });
       }
     }
-
-    if (!passwordControl?.value) {
-      this.passwordErrorMessage = 'Campo senha é obrigatório';
-    } else if (passwordControl?.invalid) {
-      this.passwordErrorMessage = 'O campo senha deve ter pelo menos 8 caracteres';
-    } else {
-      const email = emailControl?.value ?? '';
-      const password = passwordControl.value;
-
-      this.authService.authenticateUser(email, password)
-        .then(isAuthenticated => {
-          if (isAuthenticated) {
-            this.router.navigateByUrl('/home');
-          } else {
-            this.passwordErrorMessage = 'Senha incorreta';
-          }
-        });
-    }
-  }}
+  }
+}
