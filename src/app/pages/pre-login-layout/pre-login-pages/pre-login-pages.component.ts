8d651d3e7a5249036b3cb1af203d94a97eb5c86c
Barbara <barbaracantunes@gmail.com>
2023-04-02 20:18:12 -0300

    Ajustes layout pré-login

--- src/app/pages/pre-login-layout/pre-login-pages/pre-login-pages.component.ts
+++ src/app/pages/pre-login-layout/pre-login-pages/pre-login-pages.component.ts
@@ -4,18 +4,18 @@ import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-pre-login-pages',
   templateUrl: './pre-login-pages.component.html',
-  styleUrls: ['./pre-login-pages.component.css']
-})  
+  styleUrls: ['./pre-login-pages.component.css'],
+})
 export class PreLoginPagesComponent implements OnInit {
-    path: string;
-  
-    constructor(private router: Router, private activatedRoute: ActivatedRoute) {
-      this.path = '';
-    }
-  
-    ngOnInit(): void {
-      this.activatedRoute.url.subscribe(url => {
-        this.path = url[0]?.path || '';
-      });
-    }
+  path: string;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.path = '';
   }
+
+  ngOnInit(): void {
+    this.activatedRoute.url.subscribe((url) => {
+      this.path = url[0]?.path || '';
+    });
+  }
+}
