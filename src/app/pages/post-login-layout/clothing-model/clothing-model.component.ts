3fc47514cca8736ea944891145921e68946194ad
Barbara <barbaracantunes@gmail.com>
2023-04-02 20:43:49 -0300

    Adiciona pag Modelos

--- src/app/pages/post-login-layout/clothing-model/clothing-model.component.ts
+++ src/app/pages/post-login-layout/clothing-model/clothing-model.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClothingModelServiceService } from 'src/app/services/clothing-model-service.service';
 
 @Component({
   selector: 'app-clothing-model',
   templateUrl: './clothing-model.component.html',
-  styleUrls: ['./clothing-model.component.css']
+  styleUrls: ['./clothing-model.component.css'],
 })
-export class ClothingModelComponent {
+export class ClothingModelComponent implements OnInit {
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+    this.listClothingModel();
+  }
 
+  clothingModel: any[];
+
+  constructor(private clothingModelService: ClothingModelServiceService) {
+    this.clothingModel = [];
+  }
+
+  listClothingModel() {
+    this.clothingModelService.getClothingModel().subscribe((data) => {
+      this.clothingModel = data;
+    });
+  }
 }
