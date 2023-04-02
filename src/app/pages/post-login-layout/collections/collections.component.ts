72af818f3ea5f1aad26fe44182f84c597f483a53
Barbara <barbaracantunes@gmail.com>
2023-04-02 20:28:02 -0300

    Adiciona pag coleções

--- src/app/pages/post-login-layout/collections/collections.component.ts
+++ src/app/pages/post-login-layout/collections/collections.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CollectionServiceService } from '../../../services/collection-service.service';
 
 @Component({
   selector: 'app-collections',
   templateUrl: './collections.component.html',
-  styleUrls: ['./collections.component.css']
+  styleUrls: ['./collections.component.css'],
 })
-export class CollectionsComponent {
+export class CollectionsComponent implements OnInit {
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+    this.listCollection();
+  }
 
+  collection: any[];
+
+  constructor(private collectionService: CollectionServiceService) {
+    this.collection = [];
+  }
+
+  listCollection() {
+    this.collectionService.getCollection().subscribe((data) => {
+      this.collection = data;
+    });
+  }
 }
