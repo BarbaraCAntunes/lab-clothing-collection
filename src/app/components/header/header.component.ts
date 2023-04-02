5d8d41f01adb4e9dad3a9aa463cbea2d92e4d2b6
Barbara <barbaracantunes@gmail.com>
2023-04-02 20:55:58 -0300

    Add menu dropdown

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private authService: AuthService, private router: Router) {} // Injete AuthService e Router
 
+  logoutButton(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  notificacao() {
+    alert('Você não possui novas notificações!');
+  }
 }
