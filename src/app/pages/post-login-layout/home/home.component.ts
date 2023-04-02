ade83f4ca4efd0922c3c64d0cef74b36b985b8a0
Barbara <barbaracantunes@gmail.com>
2023-04-02 20:11:37 -0300

    Add lógica e revisa outros códigos

--- src/app/pages/post-login-layout/home/home.component.ts
+++ src/app/pages/post-login-layout/home/home.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  totalCollections: number = 0;
+  totalClothingModels: number = 0;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+    this.fetchCollections();
+    this.fetchClothingModels();
+  }
+
+  fetchCollections() {
+    this.http.get<any[]>('http://localhost:3000/colecoes').subscribe((data) => {
+      this.totalCollections = data.length;
+    });
+  }
+
+  fetchClothingModels() {
+    this.http.get<any[]>('http://localhost:3000/modelos').subscribe((data) => {
+      this.totalClothingModels = data.length;
+    });
+  }
 }
